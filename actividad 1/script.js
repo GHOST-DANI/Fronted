6282fa8e88064535c004b4766e604c60045d0b77
Daniel Perdomo <josedanielp82@gmail,com>
2025-11-08 15:50:55 -0500

    actualisasion

--- actividad 1/script.js
+++ actividad 1/script.js
@@ -8,16 +8,19 @@ function toggleh1(tecnologia) {
       contenido = `
         <h2>HTML</h2>
         <p>HTML es el lenguaje de marcado que estructura el contenido de la web.</p>
+        <img src="html.png">
       `;
     } else if (tecnologia === "CSS") {
       contenido = `
         <h2>CSS</h2>
         <p>CSS se usa para dar estilo, color y diseño a las páginas web.</p>
+        <img src="css.png">
       `;
     } else if (tecnologia === "JS") {
       contenido = `
         <h2>JavaScript</h2>
         <p>JavaScript agrega interactividad y dinamismo a las páginas web.</p>
+        <img src="js.png">
       `;
     }
 
